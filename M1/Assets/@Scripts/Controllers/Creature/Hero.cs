8ecaa9d2df1967c5cb4c2ab34aba0352082bdb64
jaehong.jeong <73215931+jaehong.jeong@users.noreply.github.com>
2024-07-04 00:22:24 +0900

    update script
    bug fix

--- M1/Assets/@Scripts/Controllers/Creature/Hero.cs
+++ M1/Assets/@Scripts/Controllers/Creature/Hero.cs
@@ -218,7 +218,7 @@ public class Hero : Creature
         {
             Vector3 dir = HeroCampDest.position - transform.position;
             float stopDistanceSqr = HERO_DEFAULT_STOP_RANGE * HERO_DEFAULT_STOP_RANGE;
-            if (dir.sqrMagnitude <= HERO_DEFAULT_STOP_RANGE)
+            if (dir.sqrMagnitude <= stopDistanceSqr)
             {
                 HeroMoveState = EHeroMoveState.None;
                 CreatureState = ECreatureState.Idle;
